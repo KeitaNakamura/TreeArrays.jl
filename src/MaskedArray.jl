3f503663b4e520b8488e3db0c3cdda701ef475ec
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-06-28 18:35:36 +0900

    Refactoring of MaskedArray

--- src/MaskedArray.jl
+++ src/MaskedArray.jl
@@ -17,12 +17,12 @@ Base.IndexStyle(::Type{<: MaskedArray}) = IndexLinear()
 Base.size(x::MaskedArray) = size(x.data)
 
 @inline Base.isassigned(x::MaskedArray, i::Int...) = (@_propagate_inbounds_meta; isactive(x, i...))
-@inline isactive(x::MaskedArray, i...) = (@_propagate_inbounds_meta; x.mask[i...])
+@inline isactive(x::MaskedArray, i::Int...) = (@_propagate_inbounds_meta; x.mask[i...])
 allactive(x::MaskedArray) = all(x.mask)
 anyactive(x::MaskedArray) = any(x.mask)
 
-@inline checkmask(::Type{Bool}, x::MaskedArray, i...) = (@_propagate_inbounds_meta; isactive(x, i...)) # checkbounds as well
-@inline checkmask(x::MaskedArray, i...) = (@_propagate_inbounds_meta; checkmask(Bool, x, i...) ? nothing : throw(UndefRefError()))
+@inline checkmask(::Type{Bool}, x::MaskedArray, i::Int...) = (@_propagate_inbounds_meta; isactive(x, i...)) # checkbounds as well
+@inline checkmask(x::MaskedArray, i::Int...) = (@_propagate_inbounds_meta; checkmask(Bool, x, i...) || throw(UndefRefError()))
 
 @inline function Base.getindex(x::MaskedArray, i::Int)
     @boundscheck checkbounds(x, i)
